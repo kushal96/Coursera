ca19caefa03297e0549943ed5af3356c382fddc3
Kushal Reshamdalal <kushalreshamdalal@Kushals-MacBook-Pro.local>
2020-05-10 22:30:48 -0230

    course completion

--- Algorithmic Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ Algorithmic Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -2,14 +2,39 @@ import java.util.*;
 
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
-        //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+
+        // Max value derived from Knapsack Capacity W by using items i to w.length;
+        int [][] MAX = new int [W + 1][w.length + 1];
+
+        // OPTIMAL SUBPROBLEM -> 2 Choices Either Keep Ith item or NOT
+        // IF Keep Ith Item => Max Value = MaxValue[W - w[i]][i-1] + v[i]; i -> i -1 Reduce W -> W -w[i]
+        // IF NOT KEEP Ith item => Max Value = MaxValue[W][i-1] ie i -> i-1 W -> W (same)
+
+
+        for (int weight = 0; weight <= W; weight++) {
+            for (int i = 0; i<= w.length; i++) {
+
+                if (i == 0 || weight == 0) {
+                    // Item 0 or weight 0
+                    MAX[weight][i] = 0;
+                } else {
+                    int dontUseIthItem = MAX[weight][i-1];
+
+                    // Maximising Problem -> Need to Set to - INFINITY
+                    int useIthItem = Integer.MIN_VALUE;
+
+                    // Check if there is enough weight for ith Item
+                    // ith Item's weight is w[i-1]
+                    if (weight - w[i-1] >= 0) {
+                        useIthItem = MAX[weight - w[i-1]][i-1] + w[i-1];
+                    }
+
+                    MAX[weight][i] = Math.max(useIthItem, dontUseIthItem);
+                }
+            }
         }
-        return result;
+
+        return MAX[W][w.length];
     }
 
     public static void main(String[] args) {
@@ -24,4 +49,5 @@ public class Knapsack {
         System.out.println(optimalWeight(W, w));
     }
 }
+
 
